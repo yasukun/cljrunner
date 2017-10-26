4e0932c1c71cd8cb7426d2f17380e2dbe0316adc
miyazaki <miyazaki_masayasu_f5@lab.ntt.co.jp>
2017-10-26 17:38:14 +0900

    first add

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+extern crate getopts;
+
+use getopts::Options;
+use std::env;
+
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
+    println!("Hello, world!");
+}
